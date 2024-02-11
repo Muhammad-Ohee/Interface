21e6063914d36c71abe5761d8d0bcb61493f8d74
Muhammad-Ohee <md.ohee86@gmail.com>
2024-02-11 08:16:47 +0600

    version 3

---
+++ MultipleInheritanceInRealTime/MultipleInheritanceInRealTime/LiquidInkjetPrinter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritanceInRealTime
+{
+    public class LiquidInkjetPrinter : IPrinterTasks
+    {
+        public void Print(string PrintContent)
+        {
+            Console.WriteLine(PrintContent);
+        }
+        public void Scan(string ScanContent)
+        {
+            Console.WriteLine(ScanContent);
+        }
+    }
+}
---
+++ MultipleInheritanceInRealTime/MultipleInheritanceInRealTime/Program.cs
@@ -0,0 +1,24 @@
+﻿//Console.WriteLine("Hello, OS");
+
+namespace MultipleInheritanceInRealTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("OS");
+
+            Console.WriteLine("HP Laser Jet Printer:");
+            HPLaserJetPrinter hPLaserJetPrinter = new HPLaserJetPrinter();
+            hPLaserJetPrinter.Scan("Scan Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.Print("Print Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.Fax("Fax Services by HPLaserJetPrinter");
+            hPLaserJetPrinter.PrintDuplex("Print Duplex Services by HPLaserJetPrinter");
+
+            Console.WriteLine("\nLiquid Inkjet Printer:");
+            LiquidInkjetPrinter liquidInkjetPrinter = new LiquidInkjetPrinter();
+            liquidInkjetPrinter.Scan("Scan Services by LiquidInkjetPrinter");
+            liquidInkjetPrinter.Print("Print Services by LiquidInkjetPrinter");
+        }
+    }
+}
