4cc7572f69e7955256d1de76cd3c3603fb59993d
Muhammad-Ohee <md.ohee86@gmail.com>
2024-02-11 07:55:11 +0600

    version 2

---
+++ MultipleInterface/MultipleInterface/Program.cs
@@ -0,0 +1,38 @@
+﻿//Console.WriteLine("Hello, OS");
+
+namespace MultipleInterface
+{
+    interface ICar
+    {
+        void Drive();
+    }
+
+    interface IBike
+    {
+        void Drive();
+    }
+
+    class Program : ICar, IBike
+    {
+        void ICar.Drive() 
+        {
+            Console.WriteLine("Car Drive.");
+        }
+
+        void IBike.Drive()
+        {
+            Console.WriteLine("Bike drive.");
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            ((ICar)program).Drive();
+            ((IBike)program).Drive();
+
+            (program as ICar).Drive();
+            (program as IBike).Drive();
+        }
+    }
+}
